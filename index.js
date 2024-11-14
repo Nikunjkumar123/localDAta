bc9c3d9971b42489ebed406960b960025abeca25
Nikunj kumar <120407180+Nikunjkumar123@users.noreply.github.com>
2024-11-14 19:52:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const addlocationRouter  = require('./src/router/addlocationRouter.js')
 app.use(express.json());
 app.use('/api/v1',addlocationRouter)
 
-
+app.get('/', (req, res) => {
+    res.send('<a href="/api/v1/gloc">Go to location API</a>');
+});
 connectDB(process.env.URL);
 app.listen(process.env.PORt,()=>{
     console.log("connected to PORT");
