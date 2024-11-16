8cc21d9e69f131295df9713eb240b52d1cac97b4
Nikunj kumar <120407180+Nikunjkumar123@users.noreply.github.com>
2024-11-17 00:29:25 +0530

    Update connectDB.js

--- src/db/connectDB.js
+++ src/db/connectDB.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 const mongoose = require('mongoose');
 
@@ -17,23 +16,4 @@ const connectDB = async (URL) => {
     }
 };
 
-=======
-const mongoose = require('mongoose');
-
-const connectDB = async (URL) => {
-    try {
-        await mongoose.connect(URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            connectTimeoutMS: 30000,
-            socketTimeoutMS: 30000,
-            ssl: true, // Use SSL if your MongoDB server requires it
-        });
-        console.log("Connected to MongoDB");
-    } catch (error) {
-        console.error("MongoDB connection error:", error);
-    }
-};
-
->>>>>>> d5c24e394e533aca286ee447d9314eb7d934045d
 module.exports = connectDB;
